a06d277a3f7aeec7f3d6a3684699292714cdd658
mickey-bob <54576362+mickey-bob@users.noreply.github.com>
2024-06-06 20:38:27 +0700

    Add files via upload
    update deme scraping

---
+++ demo-scraping.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import csv
+
+# initializa the data structure to store the scrapped data
+products = []
+
+#  initialize the list of discoverd urls
+urls_craw = ['https://www.scrapingcourse.com/ecommerce/','https://www.scrapingcourse.com/ecommerce/page/2/']
+# print(response.content)
+
+# util all pages have been visited
+while len(urls_craw) != 0:
+  
+  # get page to visit from the list
+  current_url = urls_craw.pop()
+  # print(current_url)
+  response = requests.get(current_url)
+  test_soup = BeautifulSoup(response.content, "html.parser")
+  # print(soup)
+  link_elements = test_soup.select("a[href]")
+  # link_elements = soup.descendants
+  # print(link_elements)
+  # check link href to avoid empty and external urls
+  urls = []
+  a_hrefs = []
+  for link_element in link_elements:
+    product = {}
+    if link_element.h2 and link_element.span and link_element.img:
+      print(link_element)
+      print('==='*20)
+      product['url'] = link_element['href']
+      product["name"] = link_element.h2.get_text()
+      product['image'] = link_element.img['src']
+      product['price'] = link_element.span.get_text()
+      products.append(product)
+
+# initialize the csv output file
+with open('products.csv', 'w') as csv_file:
+  writer = csv.writer(csv_file)
+
+  # populating the CSV
+  for product in products:
+      writer.writerow(product.values())
+      
+# print(products)
